44b24559fb5702914bf418d38a62970a4526f23c
Галин Киров <g_9635@abv.bg>
2016-09-01 15:19:26 +0300

    Модул 06, Видео 06 - Форматиране на конзолно приложение

---
+++ MP01/Colours/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Colours
+{
+	public class ForCLI
+	{
+		public ForCLI ()
+		{
+		}
+
+		public void Default ()
+		{
+			Console.ForegroundColor = ConsoleColor.Gray;
+		}
+
+		public void Prompt ()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+		}
+
+		public void Command ()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+		}
+
+		public void Result ()
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+
+	}
+}
+
